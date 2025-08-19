3fc2b370994d76f5b4c1fd39c62e97eb1813ae79
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-19 17:24:48 +0200

    fix: optimise Partner component

--- features/partners/Partners.tsx
+++ features/partners/Partners.tsx
@@ -10,37 +10,29 @@ const Partners = () => {
     { src: "/images/bmw.png", alt: "BMW" },
     { src: "/images/audi.png", alt: "Audi" },
     { src: "/images/kia-logo.png", alt: "Kia" },
+    { src: "/images/bmw.png", alt: "BMW" },
+    { src: "/images/audi.png", alt: "Audi" },
+    { src: "/images/kia-logo.png", alt: "Kia" },
+    { src: "/images/bmw.png", alt: "BMW" },
+    { src: "/images/audi.png", alt: "Audi" },
+    { src: "/images/kia-logo.png", alt: "Kia" },
   ];
 
   return (
-    <div
-      className="absolute bottom-1 xl:bottom-3 2xl:bottom-4 3xl:bottom-6 left-0 right-0 z-10 h-[100px] overflow-x-auto overflow-y-hidden
-      w-full"
-      style={{
-        scrollbarWidth: "none",
-        msOverflowStyle: "none",
-      }}
-    >
+    <div className="absolute bottom-1 xl:bottom-3 2xl:bottom-4 3xl:bottom-6 left-0 right-0 z-10 h-[100px] overflow-x-auto overflow-y-hidden scrollbar-hide">
       <div className="flex items-center justify-center py-4 px-0 min-w-max">
-        {partners.map((p, index) => (
+        {partners.map((p) => (
           <Link
             href="/"
             key={p.alt}
-            className="flex items-center    flex-shrink-0"
-            style={{ marginRight: index < partners.length - 1 ? "30px" : "0" }}
+            className="flex items-center flex-shrink-0 mr-3 last:mr-0"
           >
             <Image
               src={p.src}
               alt={p.alt}
               width={82}
-              height={35}
-              style={{
-                maxHeight: "40px",
-                maxWidth: "82px",
-                height: "auto",
-                width: "auto",
-              }}
-              className="logo-white"
+              height={40}
+              className="logo-white object-cover w-full h-full max-w-[82px] max-h-[40px]"
             />
           </Link>
         ))}
