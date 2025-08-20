5215b5699291e6374841c25aa5daaba5fd5663d7
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-20 10:44:17 +0200

    fix: add links to socila icons in footer

--- components/layout/FooterSocial.tsx
+++ components/layout/FooterSocial.tsx
@@ -4,14 +4,21 @@ import {
   faTiktok,
 } from "@fortawesome/free-brands-svg-icons";
 
+import Link from "next/link";
 import FontAwesomeIcon from "../ui/FontAwesomeIcon";
 
 const FooterSocial = () => {
   return (
     <div className="flex space-x-1 w-full lg:w-auto justify-center mb-2 lg:mb-0">
-      <FontAwesomeIcon icon={faFacebookF} />
-      <FontAwesomeIcon icon={faInstagram} classname="text-xl" />
-      <FontAwesomeIcon icon={faTiktok} />
+      <Link href="https://www.facebook.com/" target="_blank">
+        <FontAwesomeIcon icon={faFacebookF} />
+      </Link>
+      <Link href="https://www.instagram.com/" target="_blank">
+        <FontAwesomeIcon icon={faInstagram} classname="text-xl" />
+      </Link>
+      <Link href="https://www.tiktok.com/" target="_blank">
+        <FontAwesomeIcon icon={faTiktok} />
+      </Link>
     </div>
   );
 };
