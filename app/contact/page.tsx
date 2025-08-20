dcdfed4de77706ffc557c7e07b961750a7d3af5d
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-20 10:56:58 +0200

    feat: Contact page basic layout without formular

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,60 @@
+import {
+  faTelegram,
+  faViber,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+
+import BackButton from "@/components/ui/BackButton";
+import Map from "@/features/contact/Map";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <main className="flex flex-col flex-1  max-w-screen-2xl mx-auto z-10 w-full py-4 px-2 2xl:px-0">
+      <BackButton />
+      <h1 className="mb-2">
+        Get in <span className="text-primary">touch</span>
+      </h1>
+
+      <div className="flex flex-wrap lg:flex-nowrap justify-between space-x-0 lg:space-x-3 space-y-3 lg:space-y-0 mb-8">
+        <div className="w-full lg:w-1/2">
+          <p>
+            Posuere ullamcorper egestas et massa. Risus habitant enim ac et
+            aliquam mi. Natoque massa massa tortor vestibulum viverra.
+            Consectetur consequat lu
+          </p>{" "}
+          #FORMULAR
+        </div>
+        <div className="w-full lg:w-1/2 space-y-3">
+          <Map />
+          <div className="flex items-center space-x-3">
+            <div>Also available on:</div>
+            <div className="grid grid-cols-3 gap-[4px]">
+              <Link href="tel:123456" target="_blank">
+                <FontAwesomeIcon
+                  icon={faViber}
+                  className="text-2xl text-primary"
+                />
+              </Link>
+              <Link href="tel:123456" target="_blank">
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="text-2xl text-primary"
+                />
+              </Link>
+              <Link href="tel:123456" target="_blank">
+                <FontAwesomeIcon
+                  icon={faTelegram}
+                  className="text-2xl text-primary"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ContactPage;
