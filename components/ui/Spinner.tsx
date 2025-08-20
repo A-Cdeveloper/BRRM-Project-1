69bc4f30899648b391123317cf170145bdbc1223
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-20 15:23:27 +0200

    fix: reprogram Map component to have loadign state Add Spinner comp

---
+++ components/ui/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="self-center justify-center items-center">
+      <div className="h-4 w-4 border-4 border-auto-primary border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Spinner;
