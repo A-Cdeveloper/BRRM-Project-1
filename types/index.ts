74bb1adb57d271dcb555693506e137530ead60d3
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-21 01:05:31 +0200

    export all types in one file

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
 export type { FormData } from "./formular.ts";
 export type { Partner } from "./partners";
 export type { Service, ServiceWithIcon } from "./service";
+export type { Vehicle } from "./vehicles";
