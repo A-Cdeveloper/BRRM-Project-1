e588798848111a16b62fc861fa9e35fbbe83cc5f
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-21 12:38:14 +0200

    fix: FontAwesomeIcon add onclick event  and aria

--- components/ui/FontAwesomeIcon.tsx
+++ components/ui/FontAwesomeIcon.tsx
@@ -6,13 +6,25 @@ import { FontAwesomeIcon as FAIcon } from "@fortawesome/react-fontawesome";
 type FontAwesomeIconProps = {
   icon: IconProp;
   classname?: string;
+  onClick?: () => void;
+  "aria-label"?: string;
+  tabIndex?: number;
 };
 
-const FontAwesomeIcon = ({ icon, classname }: FontAwesomeIconProps) => {
+const FontAwesomeIcon = ({
+  icon,
+  classname,
+  onClick,
+  "aria-label": ariaLabel,
+  tabIndex,
+}: FontAwesomeIconProps) => {
   return (
     <FAIcon
       icon={icon}
       className={`text-primary hover:text-primary/70 cursor-pointer text-lg ${classname}`}
+      onClick={onClick}
+      aria-label={ariaLabel}
+      tabIndex={tabIndex}
     />
   );
 };
