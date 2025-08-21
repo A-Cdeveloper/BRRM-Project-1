c1b45e78028802b67cbedcdda31b6dac24b04223
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-21 12:36:26 +0200

    feat: Add vehicle detail page

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,5 +1,13 @@
+import BackButton from "@/components/ui/BackButton";
+import VehicleDetail from "@/features/vechiles/vehicle/VehicleDetail";
+
 const VechicleSinglePage = () => {
-  return <div>VechicleSinglePage</div>;
+  return (
+    <main className="flex flex-col flex-1  max-w-screen-2xl mx-auto z-10 w-full py-4 px-2 2xl:px-0 relative">
+      <BackButton />
+      <VehicleDetail />
+    </main>
+  );
 };
 
 export default VechicleSinglePage;
