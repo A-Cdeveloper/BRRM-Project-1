f25d2ca9d8d9fe9336dfa82256e5004ead6aa7fd
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-23 02:59:59 +0200

    feat: add package optimization to Next.js config
    - Add optimizePackageImports for FontAwesome packages to enable tree-shaking
    - Enable SWC minifier for faster builds and better minification
    - Add removeConsole in production to clean up console.log statements
    - Improve bundle size and loading performance

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://dhx0gpexmeekh.cloudfront.net/**")],
   },
+  experimental: {
+    // Optimizacija FontAwesome paketa - učitava samo ikone koje koristite
+    optimizePackageImports: [
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/free-brands-svg-icons",
+      "@fortawesome/react-fontawesome",
+    ],
+  },
+  // Uključuje SWC minifier za brže build-ove
+  swcMinify: true,
+  // Optimizacija za bolje performanse
+  compiler: {
+    // Uklanja console.log u production
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
